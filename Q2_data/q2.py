598ca02d2a785e5b9d054de0daca0594b2ccaaa1
Tanvi <tkarandikar@gmail.com>
2020-02-14 01:17:44 +0530

    Testly

--- Q2_data/q2.py
+++ Q2_data/q2.py
@@ -19,7 +19,6 @@ with open('./Y_train.pkl', 'rb') as f:
     y_train = pickle.load(f)   #Training set for y 
 
 x_test=x_test.reshape(-1,1)
-# print(x_test.shape)
 
 v_table=np.zeros((10,10))
 b_table=np.zeros((10,10))
@@ -40,6 +39,7 @@ for degree in range (1,10):
         X = poly.fit_transform(x_train[i].reshape(-1,1))
         X_TEST = poly.fit_transform(x_test)
         reg = LinearRegression()
+
         #Train the model for the chosen training set
         reg.fit(X, y_train[i])
         y_predict = reg.predict(X_TEST)
@@ -47,29 +47,18 @@ for degree in range (1,10):
         # plot.scatter(x_train[i], y_train[i], color = 'red')
         # plot.scatter(x_train[i], reg.predict(X), color = 'blue')
         bias_sq[i]=((np.mean(y_predict) - Fox_test) ** 2)
-        # var[i]=(np.var(y_predict,axis=0))
         out[i]=y_predict
-        # arr.append(y_predict)
     
     point_mean = np.mean(out,axis=0)
-    # bias_mean[degree-1]=np.mean(point_mean)
     bias_mean[degree-1]=np.mean((point_mean-Fox_test)**2)
-    # point_var_mean = np.mean(var,axis=0)
-    # var_mean[degree-1]=np.mean(point_var_mean)
+
     point_var = np.var(out,axis=0)
-    # print(point_var)
     var_mean[degree-1]=np.mean(point_var)
 
-# bias_final=np.array(bias_final)
-# var_final=np.array(var_final)
 
 print(pd.DataFrame(var_mean))
 print(pd.DataFrame(bias_mean))
 
-# print(var_final)
-# print(bias_mean)
-
-# bias_mean[:]-=6653086
 plot.plot(bias_mean,'b',label='Bias^2')
 plot.plot(var_mean,'r',label='Variance')
 plot.xlabel('Complexity', fontsize='medium')
