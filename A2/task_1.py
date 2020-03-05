10c98bd9acda7e4438871c8172242a7bcdf4af44
Tanvi <tkarandikar@gmail.com>
2020-03-05 12:43:28 +0530

    First iteration of the entire code finished

--- A2/task_1.py
+++ A2/task_1.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def check(health1,arrows1,stamina1,health2,arrows2,stamina2,dh,da,ds):
     if (dh==health1-health2) and (da==arrows1-arrows2) and (ds==stamina1-stamina2):
@@ -76,3 +77,88 @@ def dodge (h1,a1,s1,h2,a2,s2):
             return 0.2*0.2
 
     return 0
+
+def recharge (h1,a1,s1,h2,a2,s2):
+
+    #stamina is 100
+    if(s1==100):
+
+        if check(h1,a1,s1,h2,a2,s2,0,0,0)==1:
+            return 1
+        else:
+            return 0
+
+    #stamina not 100
+    else:
+        if check(h1,a1,s1,h2,a2,s2,0,0,50):
+            return 0.8
+        elif check(h1,a1,s1,h2,a2,s2,0,0,0):
+            return 0.2
+
+        else:
+            return 0
+
+def mapactions(ac):
+    if ac==0:
+        return "shoot"
+    elif ac==1:
+        return "dodge"
+    elif ac==2:
+        return "recharge"
+
+def callaction(ac,h1,a1,s1,h2,a2,s2):
+
+    if ac==0:
+        return shoot(h1,a1,s1,h2,a2,s2)
+
+    elif ac==1:
+        return dodge(h1,a1,s1,h2,a2,s2)
+
+    elif ac==2:
+        return recharge(h1,a1,s1,h2,a2,s2)
+
+
+penalty=[-20,-20,-20] #step cost
+happyreward=10
+gamma=0.99
+delta=0.001
+
+actions=3 #shoot,dodge,recharge
+health=5 #mult by 25
+arrows=4
+stamina=3 #mult by 50
+
+states=np.zeros((health,arrows,stamina))
+actions=np.zeros((health,arrows,stamina))
+
+#numpy.copyto(dst, src)
+deltacheck=100000000
+
+while(deltacheck>delta):
+
+    temparray=np.zeros((health,arrows,stamina))
+    temparray[:]=-100000000
+    deltacheck=-100000000
+    actions[:]=-1
+
+    for h1 in range(0,health):
+        for a1 in range(0,arrows):
+            for s1 in range(0,stamina):
+                #for each state
+
+                for ac in range(0,actions):
+                    # for each action
+                    temp=penalty[ac]
+                    for h2 in range(0,health):
+                        for a2 in range(0,arrows):
+                            for s2 in range(0,stamina):
+                                temp+=callaction(ac,h1,a1,s1,h2,a2,s2)*states[h2,a2,s2]
+
+                    if(temp>temparray[h1,a1,s1]):
+                        temparray[h1,a1,s1]=temp
+
+                if(abs(temparray[h1,a1,s1]-states[h1,a1,s1])>deltacheck):
+                    deltacheck=abs(temparray[h1,a1,s1]-states[h1,a1,s1])
+                    actions[h1,a1,s1]=ac
+
+    np.copyto(states, temparray)
