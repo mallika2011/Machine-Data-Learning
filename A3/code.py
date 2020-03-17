94666aa92c2484411669f046698268c3fadd609d
Tanvi <tkarandikar@gmail.com>
2020-03-17 15:28:00 +0530

    first draft of mutation and crossover

--- A3/code.py
+++ A3/code.py
@@ -1,5 +1,35 @@
 import numpy as np
 import tester as server
+import random
 
 MAX_DEG=3 #number of features
 key='847FWwSwTAxKTPvfixjzNnxbeudiTzJ9psoVIdUxqehtQ5efNo'
+range=10
+
+def mutation(vector,index=-1):
+    #chooses a random float in -range to +range and makes change at index position in vector
+
+    #if no index is passed chooses a random index
+    if index==-1:
+        index=random.randint(0,MAX_DEG-1)
+
+    vector[index]=random.uniform(-range,range)
+    return vector
+
+def crossover(vector1, vector2, index=-1):
+    #performs a crossover from index onwards
+
+    #if no index is passed chooses a random index
+    if index==-1:
+        index=random.randint(0,MAX_DEG-1)
+
+    send1=[]
+    send2=[]
+    for i in range(MAX_DEG):
+        if i<index:
+            send1.append(vector1[i])
+            send2.append(vector2[i])
+        else:
+            send1.append(vector2[i])
+            send2.append(vector1[i])
+    return send1, send2
