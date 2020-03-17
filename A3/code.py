c7a652c50845cfa8d6815fc0c2b3c68f672df602
Tanvi <tkarandikar@gmail.com>
2020-03-17 23:04:45 +0530

    another method

--- A3/code.py
+++ A3/code.py
@@ -20,16 +20,31 @@ def crossover(vector1, vector2, index=-1):
     #performs a crossover from index onwards
 
     #if no index is passed chooses a random index
+    send1=[]
+    send2=[]
+
     if index==-1:
         index=random.randint(0,MAX_DEG-1)
 
-    send1=[]
-    send2=[]
-    for i in range(MAX_DEG):
-        if i<index:
-            send1.append(vector1[i])
-            send2.append(vector2[i])
-        else:
-            send1.append(vector2[i])
-            send2.append(vector1[i])
-    return send1, send2
+        for i in range(MAX_DEG):
+            parity=random.randint(0,1)
+            if parity==1:
+                send1.append(vector1[i])
+                send2.append(vector2[i])
+            else:
+                send1.append(vector2[i])
+                send2.append(vector1[i])
+        return send1, send2
+
+    else:
+        for i in range(MAX_DEG):
+            if i<index:
+                send1.append(vector1[i])
+                send2.append(vector2[i])
+            else:
+                send1.append(vector2[i])
+                send2.append(vector1[i])
+        return send1, send2
+
+# how to choose individuals for crossover
+# how to choose the next gen
