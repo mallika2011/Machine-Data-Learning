d6dd24e546b8c137b3e29a24804f948196060946
Tanvi <tkarandikar@gmail.com>
2020-03-17 23:33:07 +0530

    workflow

--- A3/code.py
+++ A3/code.py
@@ -4,7 +4,8 @@ import random
 
 MAX_DEG=3 #number of features
 key='847FWwSwTAxKTPvfixjzNnxbeudiTzJ9psoVIdUxqehtQ5efNo'
-range=10
+ranger=10
+pc=0.2 
 
 def mutation(vector,index=-1):
     #chooses a random float in -range to +range and makes change at index position in vector
@@ -13,7 +14,10 @@ def mutation(vector,index=-1):
     if index==-1:
         index=random.randint(0,MAX_DEG-1)
 
-    vector[index]=random.uniform(-range,range)
+    #here probability is 0.5
+    parity=random.randint(0,1)
+    if parity==1:
+        vector[index]=random.uniform(-ranger,ranger)
     return vector
 
 def crossover(vector1, vector2, index=-1):
@@ -46,5 +50,31 @@ def crossover(vector1, vector2, index=-1):
                 send2.append(vector1[i])
         return send1, send2
 
+def choose(options,size=1):
+    send=[]
+
+    for vec in options()
+        server.get_errors(id,)
+
+
 # how to choose individuals for crossover
 # how to choose the next gen
+
+
+#main
+
+#generate initial genration (mutation) size 100
+
+#while loop
+    #create a utility array that stores the error for every individual in the population
+    #parenterrors[]
+    #get the probability of selection for every parent for going into crossover, this will generate the probaliliites based on the error
+    #above function will return an array: parentprobality
+
+    #for i in range(number of time you want to do crossover):
+    #   pick two parents using numpy.random.choice
+    #   cross them over, now we have two new
+    #   choose between them. how? -> top two? according to probability?
+    #   for now, choose only the children ignore the parents
+    #   mutate the crossovers
+    #   put them into an array called nextgen[]
