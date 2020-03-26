8231f1b1ab96d59e8daa6e8d2b92ff1886eea0d3
Tanvi <tkarandikar@gmail.com>
2020-03-26 22:29:06 +0530

    adding np.copy and correcting method of choosing next gen

--- A3/fresh.py
+++ A3/fresh.py
@@ -1,6 +1,6 @@
 import numpy as np
-# import tester as server
-import client_moodle as server
+import tester as server
+# import client_moodle as server
 import random
 
 team_name = "team_62"  # for our reference
@@ -10,10 +10,10 @@ ranger = 10
 pc = 0.2
 pop_size = 30
 select_sure = 5
-cross_select_from = 15
+cross_select_from = 10
 cross_n = int(pop_size/2)
 iter = 30
-
+prob_mut_cross=0.5
 
 # def mutation(vector, index=-1, mut_prob=0.2):  # TODO: Changed mutate_prob to 0.2
 #     if index == -1:
@@ -62,25 +62,25 @@ def crossover(vector1, vector2, index=-1):
     a = np.random.choice(np.arange(0, 11), 5, replace=False)
 
     for i in a.tolist():
-        send1[i] = vector2[i]
-        send2[i] = vector1[i]
+        send1[i] = np.copy(vector2[i])
+        send2[i] = np.copy(vector1[i])
 
-    return mutateall(send1,0.5), mutateall(send2,0.5)
-
-
-def gen_parent_probabilities(size):
-    parentprobalities = np.zeros(size)
-    for j in range(size-1):
-        parentprobalities[j] = ((1-pc)**j)*pc
-    parentprobalities[size-1] = ((1-pc)**(size-1))
-    return parentprobalities
+    return mutateall(send1,prob_mut_cross), mutateall(send2,prob_mut_cross)
 
 
-def crossover_select(parentprobalities):
-    parents = []
-    parents = np.random.choice(
-        np.arange(0, pop_size), 2, replace=False, p=parentprobalities)
-    return parents
+# def gen_parent_probabilities(size):
+#     parentprobalities = np.zeros(size)
+#     for j in range(size-1):
+#         parentprobalities[j] = ((1-pc)**j)*pc
+#     parentprobalities[size-1] = ((1-pc)**(size-1))
+#     return parentprobalities
+
+
+# def crossover_select(parentprobalities):
+#     parents = []
+#     parents = np.random.choice(
+#         np.arange(0, pop_size), 2, replace=False, p=parentprobalities)
+#     return parents
 
 
 def crossover_select2(parenterrors, num):
@@ -100,8 +100,8 @@ def check_match(vector1, vector2):
 
 
 def main():
-    print("PC: ", pc, " POP_SIZE: ", pop_size,
-          " ITER : ", iter, "Stop : ", "50")
+    print("pop_size:", pop_size, "iter:", iter, "cross_select_from",cross_select_from)
+    print("select_sure",select_sure,"prob_mut_cross",prob_mut_cross)
 
     vector_og = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -
                  6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
@@ -113,13 +113,13 @@ def main():
     parenterrors = np.zeros(pop_size)
     parenterrors1 = np.zeros(pop_size)
     parenterrors2 = np.zeros(pop_size)
-    parentprobalities = np.zeros(pop_size)
+    # parentprobalities = np.zeros(pop_size)
     population = np.zeros((pop_size, MAX_DEG))
 
     # generate the population
     for i in range(pop_size):
         temp = np.copy(vector_og)
-        population[i] = mutateall(temp,0.85)
+        population[i] = np.copy(mutateall(temp,0.85))
 
     # generate errors for each individual in the population
     for j in range(pop_size):
@@ -128,9 +128,9 @@ def main():
         err = server.get_errors(key, temp)
 
         # adding the two errors and storing in parenterror
-        parenterrors[j] = (err[0]+err[1])
-        parenterrors1[j] = (err[0])
-        parenterrors2[j] = (err[1])
+        parenterrors[j] = np.copy((err[0]+err[1]))
+        parenterrors1[j] = np.copy((err[0]))
+        parenterrors2[j] = np.copy((err[1]))
 
     # have to change this to a while loop with appropriate condition later
     for iter_num in range(iter):
@@ -138,20 +138,20 @@ def main():
         print("\n\n\n\n********"+str(iter_num)+"*********")
 
         parenerrorsinds = parenterrors.argsort()
-        parenterrors = parenterrors[parenerrorsinds[::1]]
-        parenterrors1 = parenterrors1[parenerrorsinds[::1]]
-        parenterrors2 = parenterrors2[parenerrorsinds[::1]]
-        population = population[parenerrorsinds[::1]]
+        parenterrors = np.copy(parenterrors[parenerrorsinds[::1]])
+        parenterrors1 = np.copy(parenterrors1[parenerrorsinds[::1]])
+        parenterrors2 = np.copy(parenterrors2[parenerrorsinds[::1]])
+        population = np.copy(population[parenerrorsinds[::1]])
 
         # debug statements
         for j in range(pop_size):
-            print("person " + str(j)+" error " + str(parenterrors[j]))
-            print("person " + str(j)+" error " + str(parenterrors1[j]))
-            print("person " + str(j)+" error " + str(parenterrors2[j]))
+            print("person " + str(j)+" errorfunc" + str(parenterrors[j]))
+            print("person " + str(j)+" error1" + str(parenterrors1[j]))
+            print("person " + str(j)+" error2" + str(parenterrors2[j]))
             print("\tvalues"+str(population[j])+"\n\n")
 
         # Assign probabilities to the population
-        parentprobalities = gen_parent_probabilities(pop_size)
+        # parentprobalities = gen_parent_probabilities(pop_size)
 
         child_population = np.zeros((pop_size, MAX_DEG))
         new_iter = 0
@@ -188,59 +188,83 @@ def main():
             err = server.get_errors(key, temp)
 
             # adding the two errors and storing in parenterror
-            childerrors[j] = (err[0]+err[1])
-            childerrors1[j] = (err[0])
-            childerrors2[j] = (err[1])
+            childerrors[j] = np.copy((err[0]+err[1]))
+            childerrors1[j] = np.copy((err[0]))
+            childerrors2[j] = np.copy((err[1]))
 
         # Sort children
-        childinds = childerrors.argsort()
-        childerrors = childerrors[childinds[::1]]
-        childerrors1 = childerrors1[childinds[::1]]
-        childerrors2 = childerrors2[childinds[::1]]
-        child_population = child_population[childinds[::1]]
+        childinds = np.copy(childerrors.argsort())
+        childerrors = np.copy(childerrors[childinds[::1]])
+        childerrors1 = np.copy(childerrors1[childinds[::1]])
+        childerrors2 = np.copy(childerrors2[childinds[::1]])
+        child_population = np.copy(child_population[childinds[::1]])
         # TODO: Select the best select_sure number of parents and chilren [select these many parents and children for sure]
 
+        # now the children are sorted and stored in child and parents are sorted in population
+        # we will now create a tempbank array to store top k parents, top k childs and rest being sorted taking from the top
+        tempbankerr = np.zeros(pop_size)
+        tempbankerr1 = np.zeros(pop_size)
+        tempbankerr2 = np.zeros(pop_size)
+        tempbank= np.zeros((pop_size, MAX_DEG))
+        
         for j in range(select_sure):
-            # Leaving the parent population untouched
-            population[j+select_sure] = child_population[j]
-            parenterrors[j+select_sure] = childerrors[j]
-            parenterrors1[j+select_sure] = childerrors1[j]
-            parenterrors2[j+select_sure] = childerrors2[j]
+            
+            #choosing the top jth parent and putting in the array
+            tempbank[j]=np.copy(population[j])
+            tempbankerr[j]=np.copy(parenterrors[j])
+            tempbankerr1[j]=np.copy(parenterrors1[j])
+            tempbankerr2[j]=np.copy(parenterrors2[j])
+            
+            #choosing the top jth child and putting it into the array 
+            tempbank[j+select_sure]=np.copy(child_population[j])
+            tempbankerr[j+select_sure]=np.copy(childerrors[j])
+            tempbankerr1[j+select_sure]=np.copy(childerrors1[j])
+            tempbankerr2[j+select_sure]=np.copy(childerrors2[j])
 
         # combining parents and children into one array
         # TODO: Concatenating remaining parents and children and selecting from them
-        candidates = np.concatenate(
-            [population[select_sure:], child_population[select_sure:]])
-        candidate_errors = np.concatenate(
-            [parenterrors[select_sure:], childerrors[select_sure:]])
-        candidate_errors1 = np.concatenate(
-            [parenterrors1[select_sure:], childerrors1[select_sure:]])
-        candidate_errors2 = np.concatenate(
-            [parenterrors2[select_sure:], childerrors2[select_sure:]])
+        candidates = np.copy(np.concatenate([population[select_sure:], child_population[select_sure:]]))
+        candidate_errors = np.copy(np.concatenate([parenterrors[select_sure:], childerrors[select_sure:]]))
+        candidate_errors1 = np.copy(np.concatenate([parenterrors1[select_sure:], childerrors1[select_sure:]]))
+        candidate_errors2 = np.copy(np.concatenate([parenterrors2[select_sure:], childerrors2[select_sure:]]))
 
         # sorting all the candidates by error
         candidate_errors_inds = candidate_errors.argsort()
-        candidate_errors = candidate_errors[candidate_errors_inds[::1]]
-        candidate_errors1 = candidate_errors1[candidate_errors_inds[::1]]
-        candidate_errors2 = candidate_errors2[candidate_errors_inds[::1]]
-        candidates = candidates[candidate_errors_inds[::1]]
+        candidate_errors = np.copy(candidate_errors[candidate_errors_inds[::1]])
+        candidate_errors1 = np.copy(candidate_errors1[candidate_errors_inds[::1]])
+        candidate_errors2 = np.copy(candidate_errors2[candidate_errors_inds[::1]])
+        candidates = np.copy(candidates[candidate_errors_inds[::1]])
 
         # TODO: Select the best popsize - 2*(select_sure)
-        cand_iter = 0
+        cand_iter = 2*select_sure-1
 
-        while(cand_iter < pop_size - 2*select_sure):
-            population[cand_iter] = candidates[cand_iter]
-            parenterrors[cand_iter] = candidate_errors[cand_iter]
-            parenterrors1[cand_iter] = candidate_errors1[cand_iter]
-            parenterrors2[cand_iter] = candidate_errors2[cand_iter]
+        while(cand_iter < pop_size):
+            tempbank[cand_iter] = np.copy(candidates[cand_iter])
+            tempbankerr[cand_iter] = np.copy(candidate_errors[cand_iter])
+            tempbankerr1[cand_iter] = np.copy(candidate_errors1[cand_iter])
+            tempbankerr2[cand_iter] = np.copy(candidate_errors2[cand_iter])
             cand_iter += 1
 
-        # set the send array by choosing the minimum from all the candidates NOTE: it may not be selected in the new population
-        if(min_error == -1 or min_error > candidate_errors[0]):
-            to_send = np.copy(candidates[0])
-            min_error = candidate_errors[0]
-            min_error1 = candidate_errors1[0]
-            min_error2 = candidate_errors2[0]
+
+        #now setting the next population
+        population=np.copy(tempbank)
+        parenterrors=np.copy(tempbankerr)
+        parenterrors1=np.copy(tempbankerr1)
+        parenterrors2=np.copy(tempbankerr2)
+
+        #we will now sort before updating min_error
+        parenerrorsinds = parenterrors.argsort()
+        parenterrors = np.copy(parenterrors[parenerrorsinds[::1]])
+        parenterrors1 = np.copy(parenterrors1[parenerrorsinds[::1]])
+        parenterrors2 = np.copy(parenterrors2[parenerrorsinds[::1]])
+        population = np.copy(population[parenerrorsinds[::1]])
+
+
+        if(min_error == -1 or min_error > population[0]):
+            to_send = np.copy(population[0])
+            min_error = np.copy(parenterrors[0])
+            min_error1 = np.copy(parenterrors1[0])
+            min_error2 = np.copy(parenterrors2[0])
             nochange=0
 
         else:
